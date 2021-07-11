00a88f22a62bda1778c6f8f2d098bbf6af8ea0ea
Tiany <353896627@qq.com>
2021-07-11 15:52:24 +0800

    feat: 初始化项目

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  if(app.isPackaged) {
+    win.loadFile('./build/index.html')
+  } else {
+    win.loadURL('http://localhost:3000/')
+    win.webContents.openDevTools()
+  }
+  
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', () => {
+    createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+})
